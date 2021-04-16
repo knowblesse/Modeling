fb8645d1b8eb50debecc19a2f20e584239e5db26
Ji Hoon Jeong <knowblesse@gmail.com>
2021-04-16 10:19:07 +0900

    plot shaded added

--- plot_shade.m
+++ plot_shade.m
@@ -1,11 +1,32 @@
-function ax = plot_shade(ax,m,s)
-plot(m,'k','LineWidth',1,'Parent',ax);
-hold(ax,'on');
-curve1 = m + s;
-curve2 = m - s;
-p1 = fill([1:size(curve1,1),size(curve1,1):-1:1],[curve1;flipud(curve2)],'r','Parent',ax);
-p1.LineStyle = 'none';
-p1.FaceAlpha = 0.5;
-p1.FaceColor = '#999';
-end
+function ax = plot_shade(ax,m,s,opt)
+    arguments
+        ax (1,1) matlab.graphics.axis.Axes
+        m (:,1) double
+        s (:,1) double
+        opt.LineStyle (1,1) string
+        opt.Color (1,1) string
+        opt.Shade (1,1) logical
+    end
+opts = struct('LineStyle','-','Color','k','Parent',ax,'LineWidth',1);
 
+if isfield(opt,'LineStyle')
+    opts.LineStyle = opt.LineStyle;
+end
+if isfield(opt,'Color')
+    opts.Color = opt.Color;
+end
+plot(m,opts);
+hold(ax,'on');
+if isfield(opt,'Shade')
+    if opt.Shade == false
+        return;
+    else    
+        curve1 = m + s;
+        curve2 = m - s;
+        p1 = fill([1:size(curve1,1),size(curve1,1):-1:1],[curve1;flipud(curve2)],'r','Parent',ax);
+        p1.LineStyle = 'none';
+        p1.FaceAlpha = 0.5;
+        p1.FaceColor = '#999';
+    end
+end
+end
