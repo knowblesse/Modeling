84998949464265e55f914db2c6c3d0cee6007b76
Ji Hoon Jeong <knowblesse@gmail.com>
2021-05-05 18:13:31 +0900

    TDmodel major error fixed + improvement
    - converting trial-based schedule into time-based schedule part is modified
    - c parameter is changed from 0.01 to 0.1
    - plotting trial based manner function is added

--- TD_model.m
+++ TD_model.m
@@ -1,43 +1,52 @@
 %% TD Model Simulation
 
-
 %% parameters
 CS.length = 4;
-US.start = 9;
-US.end = 10;
+US.start = 9; % US must be presented after the CS
+US.end = 10; 
 ITI = 100;
 
-%% dependent parameters
-Trial_length = CS.length + ITI;
+trial_length = max(CS.length,US.end) + ITI;
 
 %% Schedule
 schedule = [...
-    repmat([1,0,0,1,0,0,0,1], 70,1)...
+    repmat([1,0,0,1,0,0,0,1], 70,1);...
+    repmat([1,0,0,0,0,0,0,1], 100,1)...
     ];
 
 %% Second-order conditioning
 CS1_first = [...
-    repmat([0,1,0,0,0,0,0,0], 4, 1);...
-    repmat([0,0,0,0,0,0,0,0], 3, 1);...z
-    repmat([1,0,0,0,0,0,0,0], 4, 1);...
-    repmat([0,0,0,0,0,0,0,0], 49, 1)...
+    repmat([0,1,0,0,0,0,0,0], 8, 1);...
+    repmat([0,0,0,0,0,0,0,0], 4, 1);...
+    repmat([1,0,0,0,0,0,0,0], 8, 1);...
+    repmat([0,0,0,0,0,0,0,0], trial_length-20, 1)...
     ];
 
 %% Generate new Schedule including time factor
-
-newSchedule = zeros(Trial_length * size(schedule,1),8);
+newSchedule = zeros(trial_length * size(schedule,1),8);
 w_trial = zeros(size(schedule,1),3);
 for s = 1 : size(schedule,1)
-    newSchedule(Trial_length*(s-1)+1 : Trial_length*s,:) = [...
-        repmat([schedule(s,1:3),0,schedule(s,5:8)],CS.length,1);...
-        repmat([zeros(1,4),schedule(s,5:8)],ITI,1)];
-    newSchedule(Trial_length*(s-1)+US.start:Trial_length*(s-1)+US.end,4) = schedule(s,4);
+    temp = zeros(trial_length,8);
+    temp(1:CS.length,[1,2,3,5,6,7]) = repmat(schedule(s,[1,2,3,5,6,7]),CS.length,1);
+    temp(US.start:US.end,[4,8]) = repmat(schedule(s,[4,8]),US.end-US.start+1,1);
+    newSchedule(trial_length*(s-1)+1 : trial_length*s,:) = temp;
 end
+clearvars temp
+
+newSchedule = [...
+    newSchedule;...
+    repmat(CS1_first,50,1)...
+    ];
 
 %% Run
 totalTime = size(newSchedule,1);
+
+% The parameter that the original thesis (Sutton & Barto, 1987) used c=0.01, 
+% but this does not fit with the simulated data. And also, similar model
+% (SBmodel by Sutton & Barto, 1981) uses c values a way over .01.
+% So I used c=0.1
 beta = 0.8;
-c = 0.01;
+c = 0.1; 
 gamma = 0.95;
 
 % Initialize
@@ -64,11 +73,11 @@ for t = 1 : size(newSchedule,1)
         x_bar(t,:) = beta .* x_bar(t-1,:) + (1-beta) .* x(t-1,:); %eligibility traces
         w(t+1,:) = w(t,:) + c.*(...
             r(t) + gamma .* max(w(t,:) * x(t,:)',0) - max(w(t,:) * x(t-1,:)',0)...
-            )*x_bar(t,:);
+            )*x_bar(t,:); % during the CS presentaion, gamma value makes the delta w negative proportional to it's weight.
         test_v(t) = r(t) + gamma .* max(w(t,:) * x(t,:)',0) - max(w(t,:) * x(t-1,:)',0);
     end
     
-    if rem(t, Trial_length) == 0
+    if rem(t, trial_length) == 0
         w_trial(trial,:) = w(t,:);
         trial = trial + 1;
     end
@@ -76,35 +85,49 @@ end
 fprintf('%.2f, %.2f %.2f\n',w(end,:))
 
 %% Plot
-%plot_range = size(newSchedule,1);
-plot_range = Trial_length * 3;
+plot_range.start = 1;
+plot_range.end = 170;
 
 %% Plot All 
-figure(1);
+figure(2);
 clf;
 subplot(5,1,1);
 hold on;
-plot(x(1:plot_range,1),'b');
-plot(newSchedule(1:plot_range,4),'r');
+
+c_blue = [0.507,0.789,0.984];
+c_green = [0.465,0.827,0.402];
+
+plot(x(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,1),'Color',c_blue);
+plot(x(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,2),'Color',c_green);
+plot(newSchedule(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,4),'r');
 title('CS/US');
+legend({'CS1','CS2','US'});
 
 subplot(5,1,2);
-plot(x_bar(1:plot_range,1));
+hold on;
+plot(x_bar(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,1),'Color',c_blue);
+plot(x_bar(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,2),'Color',c_green);
 title('CS bar');
+legend({'CS1','CS2'});
 
 subplot(5,1,3);
-plot(y(1:plot_range));
+plot(y(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end));
 title('y');
 
-subplot(5,1,4);
-plot(test_v(1:plot_range,1));
-title('test v');
-
-subplot(5,1,5);
-plot(w(1:plot_range,1));
-hold on;
-plot(Trial_length:Trial_length:plot_range,w_trial(1:plot_range/Trial_length,:));
-xticks(Trial_length:Trial_length*5:plot_range);
-xticklabels(1:5:plot_range/Trial_length);
+subplot(5,1,4:5);
+%plot(w(1:plot_range,1));
+%hold on;
+line = plot(...
+    trial_length * (plot_range.start-1) + 1 : trial_length : trial_length * plot_range.end,...
+    w_trial(plot_range.start:plot_range.end,:));
+line(1).Color = c_blue;
+line(1).LineWidth = 2;
+line(2).Color = c_green;
+line(2).LineWidth = 2;
+xticks(trial_length * (plot_range.start-1) + 1 : trial_length*5 : trial_length * plot_range.end);
+xticklabels(plot_range.start:5:plot_range.end);
 title('w');
-
+xlabel('trial');
+ylabel('w');
+legend({'CS1','CS2','CS3'});
+%plot(w(trial_length*(plot_range.start-1)+1:trial_length*plot_range.end,1));
