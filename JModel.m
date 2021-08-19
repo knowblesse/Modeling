deeaa584a850e34ad8fb2da419217744c0b7ed84
Ji Hoon Jeong <knowblesse@gmail.com>
2021-08-19 18:47:46 +0900

    J Model basic form completed

--- JModel.m
+++ JModel.m
@@ -5,12 +5,14 @@ alpha = zeros(size(schedule,1),3);
 J = zeros(size(schedule,1),3);
 V_pos = zeros(size(schedule,1),3);
 V_bar = zeros(size(schedule,1),3);
-
+p = zeros(size(schedule,1),3);
 %% Parameters
-S = 0.5;
+S = 1;
 beta_ex = 0.1;
 beta_in= 0.09;
-gamma= 0.2;
+gamma= 0.5;
+baseline = 0.2;
+minimum_p = 0.01;
 
 %% Run
 for t = 1 : size(schedule,1)
@@ -21,10 +23,9 @@ for t = 1 : size(schedule,1)
     Lambda = US .* schedule(t,5);
     Lambda_bar = sum(CS .* V_dot) - Lambda;
 
-    baseline = 0.3;
-    p = max(1 - abs(min(max(CS .* V_dot,0),1) - Lambda), 0.05);
+    p(t,:) = max(1 - abs(min(max(CS .* V_dot,0),1) - Lambda), minimum_p);
     
-    J(t+1,:) = t/(t+1).*J(t,:) - 1/(t+1).*log2(p);
+    J(t+1,:) = t/(t+1).*J(t,:) - 1/(t+1).*log2(p(t,:));
 
     % alpha change
     if t == 1
@@ -45,14 +46,15 @@ for t = 1 : size(schedule,1)
     end
 
     % V change
-    deltaV_pos = [0, 0, 0];
-    deltaV_bar = [0, 0, 0];
-
-    if Lambda - sum(CS .* V_dot) > 0
-        deltaV_pos = CS .* S .* alpha(t,:) .* beta_ex .* Lambda;
-    else
-        deltaV_bar = CS .* S .* alpha(t,:) .* beta_in .* Lambda_bar;
-    end
+%     deltaV_pos = [0, 0, 0];
+%     deltaV_bar = [0, 0, 0];
+%     if Lambda - sum(CS .* V_dot) > 0
+%         deltaV_pos = CS .* S .* alpha(t,:) .* beta_ex .* Lambda;
+%     else
+%         deltaV_bar = CS .* S .* alpha(t,:) .* beta_in .* Lambda_bar;
+%     end
+    deltaV_pos = CS .* S .* alpha(t,:) .* beta_ex .* Lambda;
+    deltaV_bar = CS .* S .* alpha(t,:) .* beta_in .* Lambda_bar;
     V(t,:) = V_dot;
     V_pos(t+1,:) = V_pos(t,:) + deltaV_pos;
     V_bar(t+1,:) = V_bar(t,:) + deltaV_bar;
@@ -65,4 +67,5 @@ sim.alpha = alpha;
 sim.J = J;
 sim.V_pos = V_pos;
 sim.V_bar = V_bar;
+sim.p = p;
 end
--- JeongModel.m
+++ JeongModel.m
@@ -3,26 +3,30 @@
 % 2021 Knowblesse
 % 21AUG18
 
+addpath('helper_function');
 %% Color Constant
 CC.cert = '#9E2C6A';
 CC.unct = '#C17FB5';
 
-
 %% Experiment Schedule from the (Choi & Choi, 2021)
 
-schedule_uncertain = repmat([...
-    1,0,0,1,1;...
-    1,0,0,0,0;...
-    1,0,0,0,0;...
-    1,0,0,0,0;...
-    ],25,1);
-schedule_certain = repmat([...
-    1,0,0,1,0.25;...
-    ],100,1);
+% schedule_uncertain = repmat([...
+%     1,0,0,1,1;...
+%     1,0,0,0,0;...
+%     1,0,0,0,0;...
+%     1,0,0,0,0;...
+%     ],25,1);
+% schedule_certain = repmat([...
+%     1,0,0,1,0.25;...
+%     ],100,1);
 
-% schedule_uncertain = repmat([1,0,0,1,1],100,1);
-% schedule_certain = repmat([1,0,0,1,1],100,1);
-
+schedule_uncertain = repmat([1,0,0,1,1],100,1);
+schedule_certain = repmat([1,0,0,1,1],100,1);
+schedule_partial = repmat([...
+    1,0,0,1,0.2;...
+    1,0,0,1,0.4;...
+    1,0,0,1,0.6;...
+    1,0,0,1,0.8;],25,1);
 num_repeat = 100;
 
 %% Run
@@ -31,7 +35,7 @@ alpha = cell(1,2);
 J = cell(1,2);
 V_pos = cell(1,2);
 V_bar = cell(1,2);
-
+p = cell(1,2);
 for r = 1 : num_repeat
     % Shuffle schedule for repeated simulation
     schedule_uncertain_shuffled = [shuffle1D(schedule_uncertain);repmat([1,0,0,0,0],50,1)];
@@ -49,40 +53,49 @@ for r = 1 : num_repeat
     J{1}(:,r) = sim1.J(:,1);
     J{2}(:,r) = sim2.J(:,1);
 
-    V_pos{1}(:,r) = sim1.V_pos(:,1);
-    V_pos{2}(:,r) = sim2.V_pos(:,1);
-
-    V_bar{1}(:,r) = sim1.V_bar(:,1);
-    V_bar{2}(:,r) = sim2.V_bar(:,1);
+    p{1}(:,r) = sim1.p(:,1);
+    p{2}(:,r) = sim2.p(:,1);
 end
 
-fig1 = figure(1);
-clf;
-hold on;
-
-fig2 = figure(2);
+fig1 = figure(3);
 clf;
 hold on;
 
 [~,V_plot_1] = plot_shade(fig1.Children, mean(V{1},2), std(V{1},0,2),'Color',CC.unct,'LineWidth',2,'Shade',true);
 [~,alpha_plot_1] = plot_shade(fig1.Children, mean(alpha{1},2), std(alpha{1},0,2), 'Color',CC.unct,'LineWidth',2,'Shade',true,'LineStyle','--');
 [~,J_plot_1] = plot_shade(fig1.Children, mean(J{1},2), std(J{1},0,2), 'Color','#75C9E2','LineWidth',1,'Shade',true);
-[~,V_pos_plot_1] = plot_shade(fig1.Children, mean(V_pos{1},2), std(V_pos{1},0,2),'Color','#E3364A','Shade',true);
-[~,V_bar_plot_1] = plot_shade(fig1.Children, mean(V_bar{1},2), std(V_bar{1},0,2),'Color','#61B0E2','Shade',true);
+[~,p_plot_1] = plot_shade(fig1.Children, mean(p{1},2), std(p{1},0,2),'Color','#E3364A','Shade',true);
 xlabel('Trial');
 ylabel('V');
 xlim([0,150]);
-ylim([0,1]);
-legend([V_plot_1{1}, alpha_plot_1{1}, J_plot_1{1}, V_pos_plot_1{1}, V_bar_plot_1{1}],{'V', 'alpha', 'J', 'V_{pos}', 'V_{bar}'});
+ylim([0,2]);
+legend([V_plot_1{1}, alpha_plot_1{1}, J_plot_1{1}, p_plot_1{1}],{'V', 'alpha', 'J', 'p'});
 
+fig2 = figure(4);
+clf;
+hold on;
 
 [~,V_plot_2] = plot_shade(fig2.Children, mean(V{2},2), std(V{2},0,2),'Color',CC.unct,'LineWidth',2,'Shade',true);
 [~,alpha_plot_2] = plot_shade(fig2.Children, mean(alpha{2},2), std(alpha{2},0,2), 'Color',CC.unct,'LineWidth',2,'Shade',true,'LineStyle','--');
 [~,J_plot_2] = plot_shade(fig2.Children, mean(J{2},2), std(J{2},0,2), 'Color','#75C9E2','LineWidth',1,'Shade',true);
-[~,V_pos_plot_2] = plot_shade(fig2.Children, mean(V_pos{2},2), std(V_pos{2},0,2),'Color','#E3364A','Shade',true);
-[~,V_bar_plot_2] = plot_shade(fig2.Children, mean(V_bar{2},2), std(V_bar{2},0,2),'Color','#61B0E2','Shade',true);
+[~,p_plot_2] = plot_shade(fig2.Children, mean(p{2},2), std(p{2},0,2),'Color','#E3364A','Shade',true);
 xlabel('Trial');
 ylabel('V');
 xlim([0,150]);
-ylim([0,1]);
-legend([V_plot_2{1}, alpha_plot_2{1}, J_plot_2{1}, V_pos_plot_2{1}, V_bar_plot_2{1}],{'V', 'alpha', 'J', 'V_{pos}', 'V_{bar}'});
+ylim([0,2]);
+legend([V_plot_2{1}, alpha_plot_2{1}, J_plot_2{1}, p_plot_2{1}],{'V', 'alpha', 'J', 'p'});
+
+fig3 = figure(3);
+clf;
+hold on;
+
+sim3 = JModel(schedule_partial);
+plot(sim3.V(:,1), 'Color', 'k','LineWidth', 2);
+plot(sim3.alpha(:,1), 'Color', 'k', 'LineStyle','--');
+plot(sim3.p(:,1), 'Color', 'r');
+plot(sim3.J(:,1), 'Color', 'g');
+
+
+
+
+
