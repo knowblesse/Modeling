aff937a77f6f5b8505ed86428102185b60241174
Ji Hoon Jeong <knowblesse@gmail.com>
2022-01-03 18:29:54 +0900

    new experiments

--- VDAC/experiments/Anderson_Halpern_2017.m
+++ VDAC/experiments/Anderson_Halpern_2017.m
@@ -1,18 +1,17 @@
-%% Le Pelley 2019
-% Le Pelley et al (2019) simulation
+%% Anderson_Halpern_2017
 
 %% Experiment Data
-RT_none.T1008 = 665;
-RT_low.T1008 = 673;
-RT_high.T1008 = 681;
+RT_none.T240 = 688;
+RT_low.T240 = 689;
+RT_high.T240 = 698;
 
-Exp_high_mean = RT_high.T1008 - RT_none.T1008;
-Exp_high_sd = 2.8;
-Exp_low_mean = RT_low.T1008 - RT_none.T1008;
+Exp_high_mean = RT_high.T240 - RT_none.T240;
+Exp_high_sd = 2.8; %%%%%%%%%%%%%% 임의값
+Exp_low_mean = RT_low.T240 - RT_none.T240;
 Exp_low_sd = 2.8;
 
-exp1_high_reward = 1; %5 cents
-exp1_low_reward = 0.2; %1 cents
+exp1_high_reward = 1; %10 cents
+exp1_low_reward = 0.2; %2 cents
 
 %% Experiment Schedule
 % +--------+--------+--------+--------+---------------------------+
@@ -26,8 +25,8 @@ schedule.schedule_training = [...
     repmat([1,0,0,1,exp1_low_reward],1,1);...
     repmat([0,1,0,1,exp1_high_reward],1,1);...
     repmat([0,1,0,1,exp1_low_reward],4,1);...
-    ]; % thesis : 1008 trials vs Modeling : 1010 trials (10 trial set * 101)
-schedule.schedule_training_repeat = 101;
+    ]; % thesis : 240 trials vs Modeling : 240 trials (10 trial set * 24)
+schedule.schedule_training_repeat = 24;
 schedule.schedule_training_N = size(schedule.schedule_training,1) * schedule.schedule_training_repeat;
 
 schedule.schedule_testing = [...
@@ -35,8 +34,8 @@ schedule.schedule_testing = [...
     [0,1,0,0,0];
     [0,0,0,0,0];
     [0,0,0,0,0];
-    ]; % * 120 
-schedule.schedule_testing_repeat = 120;
+    ]; % * 60 = 240 test trials
+schedule.schedule_testing_repeat = 60;
 schedule.schedule_testing_N = size(schedule.schedule_testing,1) * schedule.schedule_testing_repeat;
 
 %% Linear Transformation Parameter
