130ce93644e3df62c6255b93626c79a12b31e09b
Knowblesse <knowblesse@gmail.com>
2022-01-13 14:11:03 +0900

    Rearranged results & unused files deleted. Now VDAC uses Multi Start

--- VDAC/evalModel.m
+++
@@ -1,8 +0,0 @@
-function likelihood = evalModel(param, V_high, V_low, Exp_high_mean, Exp_high_sd, Exp_low_mean, Exp_low_sd)
-
-Exp_high = normpdf(linspace(param(1), param(2), 30), Exp_high_mean, Exp_high_sd);
-Exp_low  = normpdf(linspace(param(1), param(2), 30), Exp_low_mean, Exp_low_sd);
-
-likelihood = - (V_high * Exp_high' + V_low * Exp_low');
-
-end
